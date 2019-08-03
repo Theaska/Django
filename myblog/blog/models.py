c868f0911b0cecfac197248a650cd4de3f070869
Theaska <tach.pu@gmail.com>
2019-08-03 18:44:20 +0400

    new file:   .vscode/settings.json 	new file:   blog/__pycache__/admin.cpython-37.pyc 	new file:   blog/__pycache__/models.cpython-37.pyc 	modified:   blog/admin.py 	new file:   blog/migrations/0001_initial.py 	new file:   blog/migrations/__pycache__/0001_initial.cpython-37.pyc 	new file:   blog/migrations/__pycache__/0002_auto_20190802_1952.cpython-37.pyc 	new file:   blog/migrations/__pycache__/0003_delete_comment.cpython-37.pyc 	new file:   blog/migrations/__pycache__/__init__.cpython-37.pyc 	modified:   blog/models.py 	modified:   myblog/__pycache__/settings.cpython-37.pyc 	modified:   myblog/settings.py 	new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\320\2652.png" 	new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\320\2653.png" 	new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\320\2654.png"

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    nickname = models.CharField(max_length=50, primary_key=True, unique=True)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f"Nickname: {self.nickname}, Email: {self.email}"
+
+
+class Post(models.Model):
+    authors = models.ManyToManyField(Author)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=400)
+    date_publish = models.DateField()
+    in_blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Title: {self.title}, date: {self.date_publish}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date_publish = models.DateField()
+    in_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.text[:30]
+
+
+
+
+
+
+
+
+
+
