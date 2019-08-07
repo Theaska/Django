0e9f77b6729c82fdbf00e9cc44ebc3e3f0005958
Theaska <tach.pu@gmail.com>
2019-08-07 19:59:07 +0400

    modified:   .vscode/settings.json 	modified:   myblog/blog/__pycache__/views.cpython-37.pyc 	modified:   myblog/blog/views.py

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,8 +4,8 @@ from .models import Post, Blog
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
-MONTH_ =   ['January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December']
+MONTH_ =   ['january', 'february', 'march', 'april', 'may', 'june',
+            'july', 'august', 'september', 'october', 'november', 'december']
 
 def index(request):
     latest_post_list = Post.objects.order_by('-date_publish')[:10]
@@ -26,8 +26,8 @@ def blog(request, blog_tag):
                                              'tg_bl': blogs_taglines})
 
 def archive(request, month):
-    if month in MONTH_:
-        posts = Post.objects.filter(date_publish__month = (MONTH_.index(month)+1))
+    if month.lower() in MONTH_:
+        posts = Post.objects.filter(date_publish__month = (MONTH_.index(month.lower())+1))
         blogs_taglines = Blog.objects.values('tagline')
         return render(request, 'blog/archive.html', {'posts': posts, 
                                                     'tg_bl': blogs_taglines})
