df7180601e824c21af75bec2ccab63a2d0c28a8c
Tatyana <tach.pu@gmail.com>
2019-08-13 15:27:45 +0400

    Update my_filters.py

--- myblog/blog/templatetags/my_filters.py
+++ myblog/blog/templatetags/my_filters.py
@@ -1,5 +1,4 @@
 from django import template
-<<<<<<< HEAD
 from blog.models import Blog
 
 register = template.Library()
@@ -14,13 +13,3 @@ def months_list(value):
 def get_blogs_tags(value):
         value = Blog.objects.values('tagline')
         return value
-=======
-
-register = template.Library()
-
-@register.filter(name='monthList')
-def monthList(value):
-    value = ['January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December']
-    return value
->>>>>>> 0e9f77b6729c82fdbf00e9cc44ebc3e3f0005958
